a0fb8f8d0a4ee2dc4c96387388cc3a9a140839b6
hquanvo <vo.hoang.quan2003@gmail.com>
2023-07-06 16:52:32 +0700

    Made minor changes in all files

--- calculator.js
+++ calculator.js
@@ -1,6 +1,14 @@
+// contains AC, C, %, +/-, .
+const specials = document.querySelectorAll(".special");
+// contains 7, 8, 9, 4, 5, 6, 1, 2, 3, 0
+const nums = document.querySelectorAll(".num");
+// contains +, -, x, /, =
+const operators = document.querySelectorAll(".operator");
+
 let first = 0;
 let operation = '';
 let second = 0;
+let displayString = first + " " + operation + " " + second;
 
 // operate
 function operate() {
