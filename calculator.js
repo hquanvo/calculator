c168a5f35c59dc2d51a15ccc95d46d27c34c8fef
hquanvo <vo.hoang.quan2003@gmail.com>
2023-07-07 10:30:24 +0700

    Add ability to input, clear display and percentage

--- calculator.js
+++ calculator.js
@@ -1,14 +1,29 @@
+// declare consts
 // contains AC, C, %, +/-, .
 const specials = document.querySelectorAll(".special");
 // contains 7, 8, 9, 4, 5, 6, 1, 2, 3, 0
 const nums = document.querySelectorAll(".num");
 // contains +, -, x, /, =
 const operators = document.querySelectorAll(".operator");
+const displayTop = document.querySelector(".display > .top");
+const displayBottom = document.querySelector(".display > .bottom");
+
+nums.forEach((num) => {
+    num.addEventListener('click', () => {
+        if (displayBottom.innerHTML === '0') {
+            displayBottom.innerHTML = '';
+        }
+            displayBottom.innerHTML += num.innerHTML;
+    })
+})
+
+specials[0].addEventListener('click', clearAll);
+specials[1].addEventListener('click', clearOne);
+specials[2].addEventListener('click', percentage);
 
 let first = 0;
 let operation = '';
 let second = 0;
-let displayString = first + " " + operation + " " + second;
 
 // operate
 function operate() {
@@ -49,4 +64,17 @@ function divide(a, b) {
     return a / b;
 }
 
+function clearAll() {
+    displayBottom.innerHTML = '0';
+}
+
+function clearOne() {
+    displayBottom.innerHTML = 
+    displayBottom.innerHTML.slice(0, displayBottom.innerHTML.length - 1);
+}
+
+function percentage() {
+    displayBottom.innerHTML = 
+    parseFloat(displayBottom.innerHTML)/100;
+}
 // TODO: add a clear function, a clear all function
