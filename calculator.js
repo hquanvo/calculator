f34016335bf2da02e9b77ff12c698bdc53139bb5
hquanvo <vo.hoang.quan2003@gmail.com>
2023-07-08 22:06:19 +0700

    Add ability to do single operation

--- calculator.js
+++ calculator.js
@@ -1,4 +1,4 @@
-// declare consts
+// declare variables
 // contains AC, C, %, +/-, .
 const specials = document.querySelectorAll(".special");
 // contains 7, 8, 9, 4, 5, 6, 1, 2, 3, 0
@@ -8,12 +8,18 @@ const operators = document.querySelectorAll(".operator");
 const displayTop = document.querySelector(".display > .top");
 const displayBottom = document.querySelector(".display > .bottom");
 
+let firstOperand = 0;
+let operation = '';
+let secondOperand = 0;
+let isOperationPreviousInput = false;
+
 nums.forEach((num) => {
     num.addEventListener('click', () => {
-        if (displayBottom.innerHTML === '0') {
+        if (displayBottom.innerHTML === '0' || isOperationPreviousInput) {
             displayBottom.innerHTML = '';
         }
-            displayBottom.innerHTML += num.innerHTML;
+        if (isOperationPreviousInput) isOperationPreviousInput = false;
+        displayBottom.innerHTML += num.innerHTML;
     })
 })
 
@@ -21,6 +27,7 @@ specials[0].addEventListener('click', clearAll);
 specials[1].addEventListener('click', clearOne);
 specials[2].addEventListener('click', percentage);
 specials[3].addEventListener('click', () => {
+    if (isOperationPreviousInput) isOperationPreviousInput = false;
     if (displayBottom.innerHTML === "0") return;
     if (displayBottom.innerHTML.startsWith("-")) {
         displayBottom.innerHTML = displayBottom.innerHTML.slice(1);
@@ -29,32 +36,36 @@ specials[3].addEventListener('click', () => {
     }
 })
 specials[4].addEventListener('click', () => {
+    if (isOperationPreviousInput) isOperationPreviousInput = false;
     if (!displayBottom.innerHTML.includes(".") && displayBottom.innerHTML != '0') {
         displayBottom.innerHTML += ".";
     }
 })
 
-let first = 0;
-let operation = '';
-let second = 0;
+operators.forEach((operator) => {
+    operator.addEventListener('click', () => {
+        handleOperatorClick(operator);
+    })
+})
+
 
 // operate
 function operate() {
     switch (operation) {
         case "+": {
-            first = add(first, second);
+            firstOperand = add(firstOperand, secondOperand);
             break;
         }
         case "-": {
-            first = subtract(first, second);
+            firstOperand = subtract(firstOperand, secondOperand);
             break;
         }
-        case "*": {
-            first = multiply(first, second);
+        case '×': {
+            firstOperand = multiply(firstOperand, secondOperand);
             break;
         }
-        case "/": {
-            first = divide(first, second)
+        case '÷': {
+            firstOperand = divide(firstOperand, secondOperand)
             break;
         }
     }
@@ -78,10 +89,16 @@ function divide(a, b) {
 }
 
 function clearAll() {
+    displayTop.innerHTML = '';
     displayBottom.innerHTML = '0';
+    firstOperand = 0;
+    operation = '';
+    secondOperand = 0;
+    isOperationPreviousInput = false;
 }
 
 function clearOne() {
+    if (isOperationPreviousInput) isOperationPreviousInput = false;
     let numsRemain;
     if (displayBottom.innerHTML.startsWith('-')) {
         numsRemain = displayBottom.innerHTML.length - 1;
@@ -92,12 +109,35 @@ function clearOne() {
         displayBottom.innerHTML = 
         displayBottom.innerHTML.slice(0, displayBottom.innerHTML.length - 1);
     } else {
-        clearAll();
+        displayBottom.innerHTML = "0";
     }
 }
 
 function percentage() {
+    if (isOperationPreviousInput) isOperationPreviousInput = false;
     displayBottom.innerHTML = 
     parseFloat(displayBottom.innerHTML)/100;
 }
-// TODO: add a clear function, a clear all function
+
+function handleOperatorClick(operator) {
+    if (operator.innerHTML === "=") {
+        // TODO: handle the equal case
+        if (operation != '' && !isOperationPreviousInput) {
+            secondOperand = parseFloat(displayBottom.innerHTML);
+        }
+        operate();
+        displayTop.innerHTML = '';
+        displayBottom.innerHTML = firstOperand;
+    } else {
+        if (operation === '') {
+            firstOperand = parseFloat(displayBottom.innerHTML);
+            isOperationPreviousInput = true;
+            operation = operator.innerHTML;
+            displayTop.innerHTML = displayTop.innerHTML + firstOperand + " " + operation;
+        }
+    }
+}
+
+function enterFirstValue() {
+    
+}
